c5548d510c420d4f04fe816585caf5bc468a2a69
base0x10 <jespy@gwu.edu>
2020-05-29 00:35:54 -0400

    Implement basic correctness testing harness
    tests.py runs a set of warriors against each other at a number of
    starting positions and checks that exhaust and pmars agree on outcome
    
    This method has lots of limitations.  If the input warriors are human
    written, then it will only test redcode features that human are likely
    to write.  Also doesn't test correctness of exhaust as an api, only
    exhaust as an executable.  Also, I am only testing the standard setup
    (CORESIZE, NUMWARRIORS, MINDISTANCE, etc...)
    
    The _right_ way to do this would be to fuzz over all redcode programs
    and over all valid core configurations and compilation options guided by
    branches executed *AND* to write unit tests for public exhaust apis.
    
    I have no plans to do the right thing.

---
+++ tests/tests.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from sys import argv
+from os import listdir, popen, path
+import random
+import concurrent.futures
+
+
+def compare_results(pmars_results, othermars_results):
+    return (
+        pmars_results[9] == othermars_results[0]
+        and pmars_results[11] == othermars_results[4]
+    )
+
+
+def run_test(pmars, othermars, w1, loadfiles, num_tests):
+    warrior_size = 100
+    min_dist = 100
+    core_size = 8000
+    for w2 in loadfiles:
+        print("Testing {} against {}".format(w1, w2))
+        for i in range(num_tests):
+            w2_offset = random.randint(warrior_size + min_dist, core_size - min_dist)
+            pmars_cmd = "{} -b -F {} {} {} | tail -n +3".format(
+                pmars, w2_offset, w1, w2
+            )
+            othermars_cmd = "{} -bk -F {} {} {}".format(othermars, w2_offset, w1, w2)
+            pmars_res = popen(pmars_cmd).read()
+            othermars_res = popen(othermars_cmd).read()
+
+            if compare_results(pmars_res, othermars_res) == False:
+                print("Found an error: \n pmars_res {}, othermars_res {}, \n cmds = {} \n {}".format(
+                    pmars_res, othermars_res, pmars_cmd, othermars_cmd
+                ))
+
+
+def run_tests(pmars, othermars, warriors, num_tests):
+    # use pmars to convert each warrior into loadfile format
+    warrior_list = list(filter(lambda x: x.endswith(".red"), listdir(warriors)))
+    loadfiles = []
+    for w in warrior_list:
+        infile = path.join(warriors, w)
+        outfile = path.join(warriors, w[:-4] + ".rc")
+        loadfiles.append(outfile)
+        cmd = 'echo ";assert 1\n" > {} && {} -r 0 {} | tail -n +2 >> {}'.format(
+            outfile, pmars, infile, outfile
+        )
+        popen(cmd).read()
+
+    # We can use a with statement to ensure threads are cleaned up promptly
+    with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+        # Start the load operations and mark each future with its URL
+        futures = [
+            executor.submit(run_test, pmars, othermars, w1, loadfiles, num_tests)
+            for w1 in loadfiles
+        ]
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
+if __name__ == "__main__":
+    if len(argv) < 5:
+        print(
+            "Usage: test-bench.py <path_to_pmars> <path_to_other_mars> <path_to_warrior_dir> <num_tests_per_warrior_pair>"
+        )
+        exit(0)
+
+    run_tests(argv[1], argv[2], argv[3], int(argv[4]))
